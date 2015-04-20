0bb819c376f26286137c7326ca64253a7d8257ce
Camron Levanger <clevanger@insidesales.com>
2015-04-20 09:50:42 -0600

    initial bot to work on, right now he only flips tables

--- isbot.py
+++ isbot.py
@@ -1,2 +1,24 @@
+import requests
+import time
+
+crontable = []
+outputs = []
+
 def process_message(data):
+	isbot = '<@U04F3NB5M>'
 	print data
+
+	# send a direct message to isbot with a command
+	if data['text'] and isbot in data['text']:
+		if 'tableflip' in data['text']:
+			tableflip = requests.get('http://tableflipper.com/json')
+			tableflip = tableflip.json()
+			print tableflip
+			outputs.append([data['channel'], tableflip['gif']])
+		else:
+			outputs.append([data['channel'], "Sorry, I do not understand"])
+			outputs.append([data['channel'], "Commands I know: tableflip"])
+
+	if data['text'] == isbot:
+		print 'someone said just isbot'
+		outputs.append([data['channel'], "Commands I know: tableflip"])
